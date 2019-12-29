44904db393eb844370226b4e4ecfc6eeb4f6c13f
Olena Tsybulska <tsybulskayallena@gmail.com>
2019-12-29 22:32:23 +0200

    Add files

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -3,7 +3,7 @@ let range = 9,
     attempts = 3,
     userPrize = 0,
     countWin = 1,
-    two = 2, three = 3, four = 4,
+    one = 1, two = 2, three = 3, four = 4, nine = 9,
     prize25 = 25, prize50 = 50, prize100 = 100,
     prizes = [prize25, prize50, prize100],
     number, userNumber, win;
@@ -11,7 +11,6 @@ let range = 9,
 if (confirm('Do you want to play a game?')) {
     while (userAgree === true) {
         number = Math.floor(Math.random() * range);
-        console.log(number);
         for (let i = attempts; i > 0 ; i--) {
             userNumber = parseInt(prompt('Choose a roulette pocket number from 0 to ' + (range - 1) +
             '\nAttempts left: ' + i +
@@ -29,13 +28,15 @@ if (confirm('Do you want to play a game?')) {
             alert('Congratulation, you won! Your prize is: ' + userPrize + '$');
             userAgree = confirm('Do you want to continue?');
             if (userAgree === true) {
-                attempts = three;
                 range += four;
                 countWin *= two;
             }
         } else {
             alert('Thank you for your participation. Your prize is: ' + userPrize + '$');
             userAgree = confirm('Do you want to play again?');
+            range = nine;
+            countWin = one;
+            userPrize = 0;
         }
     }
 } else {
