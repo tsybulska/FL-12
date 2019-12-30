943e96d1d7e43f50ba1d436502d2f9fed023a04a
Olena Tsybulska <tsybulskayallena@gmail.com>
2019-12-31 00:35:30 +0200

    Add files

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -12,9 +12,4 @@ function isLeapYear(userDate) {
     return 'Invalid Date';
 }
 
-isLeapYear('2020-01-01 00:00:00');
-isLeapYear('2020-01-01 00:00:00777');
-isLeapYear('2021-01-15 13:00:00');
 isLeapYear('2200-01-15 13:00:00');
-isLeapYear(1213131313135465656654564646542132132131);
-isLeapYear(1213131313);
